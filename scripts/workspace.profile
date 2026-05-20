6bd46c13de4c39d901fd46e6f7015a3159c534f0
Lazaro <lachy200408@gmail.com>
2026-05-20 16:57:01 -0400

    dev: Setting up the backup flow

--- scripts/workspace.profile
+++ scripts/workspace.profile
@@ -12,8 +12,15 @@ WS_HELP="
   - ${GREEN}cp_sites${NC}: Copies sites folder from the container to the local workspace
   - ${GREEN}cp_logs${NC}: Copies logs folder from the container to the local workspace
   - ${GREEN}cp_files${NC}: Copies all the folders from the container to the local workspace
+  - ${GREEN}backup${NC}: Docker compose for the backup container
+  - ${GREEN}backup-db${NC}: Backups the database
+  - ${GREEN}sync-db${NC}: Syncs the database
 
 "
+
+function comp {
+	sudo docker compose -f ./pwd.yml $@
+}
 
 function cbash {
 	comp exec $1 bash
@@ -37,8 +44,24 @@ function cp_files {
 	cp_logs
 }
 
-function comp {
-	sudo docker compose -f ./pwd.yml $@
+function backup {
+	sudo docker compose -f ./backup.yml $@
+}
+
+function backup-db {
+	sudo docker run --rm \
+		-v erpnext_dev_db-data:/var/lib/mysql \
+		-v $(pwd)/volumes:/db_volume \
+		ubuntu:22.04 \
+		bash -c "cd /var/lib/mysql; tar -cf ./mysql.tar ./; cp ./mysql.tar /db_volume/"
+}
+
+function sync-db {
+	sudo docker run --rm \
+		-v erpnext_dev_db-data:/var/lib/mysql \
+		-v $(pwd)/volumes:/db_volume \
+		ubuntu:22.04 \
+		bash -c "cd /db_volume; mkdir mysql; tar -xf /db_volume/mysql.tar -C ./mysql; chown -R 999:999 .; cp -rf ./mysql /var/lib/" 
 }
 
 function wshelp {
--- volumes/copy_files.sh
+++ volumes/copy_files.sh
@@ -1,20 +1,68 @@
 #!/bin/bash
 
 
+function pmsg {
+	echo "$1"
+	local length=${#1}
+	local i=0
+	while [ $i -lt $length ]; do
+		echo -n "_"
+		i=$((i+1))
+	done
+	echo ""
+}
+
 function xend {
-	echo "Exiting with error: $1"
+	pmsg "Exiting with error: $1"
 	exit 1
 }
 
-if command -v tar > /dev/null; then
-	echo "Extracting mysql.tar volume"
-  tar -xf ./mysql.tar ./mysql || xend "Couldnt extract mysql volume"
+function from_host_to_container {
+	if command -v tar > /dev/null; then
+		mkdir /home/frappe/extracted
 
-	echo "Extracting sites.tar volume"
-	tar -xf ./sites.tar ./sites || xend "Couldnt extract sites volume"
+		if [ -e './sites.tar' ]; then
+			cp ./sites.tar /home/frappe/extracted/
+			cd /home/frappe/extracted
+
+			pmsg "Extracting sites.tar volume..."
+			tar -xf ./sites.tar ./sites || xend "Couldnt extract sites volume"
+
+			pmsg "Copying sites files..."
+			cp -rf ./sites /home/frappe/frappe-bench/ || xend "Couldnt copy sites files"
+		fi
+
+		if [ -e './apps.tar' ]; then
+			cp ./apps.tar /home/frappe/extracted/
+			cd /home/frappe/extracted
+
+			pmsg "Extracting apps.tar volume..."
+			tar -xf ./apps.tar ./apps || xend "Couldnt extract apps volume"
+
+			pmsg "Copying apps files..."
+			cp -rf ./apps /home/frappe/frappe-bench/ || xend "Couldnt copy apps files"
+		fi
+	fi
+}
+
+function from_container_to_host {
+	if command -v tar > /dev/null; then
+		pmsg "Creating sites.tar volume..."
+		cd /home/frappe/frappe-bench/sites
+		tar -cf ./sites.tar ./ || xend "Couldnt create sites.tar volume"
+		pmsg "Copying sites.tar volume..."
+		cp -f ./sites.tar /home/frappe/volumes/
+
+		pmsg "Creating apps.tar volume..."
+		cd /home/frappe/frappe-bench/apps
+		tar -cf ./apps.tar ./ || xend "Couldnt create apps.tar volume"
+		pmsg "Copying apps.tar volume..."
+		cp -f ./apps.tar /home/frappe/volumes/
+	fi
+}
+
+if [ "$1" = "--from-host" ]; then
+	from_host_to_container
+else
+	from_container_to_host
 fi
-
-for file in "$@"; do
-  echo "Copying $file"
-  docker cp "$file" frappe:/home/frappe/frappe-bench/sites/assets/erpnext/public/files/
-done
